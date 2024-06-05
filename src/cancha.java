409e55da6ef0df3538d8aeba9f4c811b2f1986c9
IgnacioSantoro <ignacio.santoro@adviters.com>
2024-06-05 20:05:34 -0300

    Initial commit

---
+++ src/cancha.java
@@ -0,0 +1,46 @@
+public class cancha {
+
+    int id;
+    String tipodecancha;
+    int capMax;
+    boolean techada;
+
+    public cancha(int id, String tipodecancha, int capMax, boolean techada) {
+        this.id = id;
+        this.tipodecancha = tipodecancha;
+        this.capMax = capMax;
+        this.techada = techada;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipodecancha() {
+        return tipodecancha;
+    }
+
+    public void setTipodecancha(String tipodecancha) {
+        this.tipodecancha = tipodecancha;
+    }
+
+    public int getCapMax() {
+        return capMax;
+    }
+
+    public void setCapMax(int capMax) {
+        this.capMax = capMax;
+    }
+
+    public boolean isTechada() {
+        return techada;
+    }
+
+    public void setTechada(boolean techada) {
+        this.techada = techada;
+    }
+}
